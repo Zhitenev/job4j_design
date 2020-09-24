f5ec6e58a7ecfb0ca455f5536bf884dafd92a839
Oleg Zhitenev <oleg@openres.ru>
2020-09-24 10:57:24 +0700

    2. Без переопределения equals и hashCode [#327213]

--- chapter_001/src/main/java/ru/job4j/map/User.java
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -1,9 +1,22 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
+
+    public static void main(String[] args) {
+        User userOne = new User();
+        User userTwo = new User();
+        Map<User, Object> map = new HashMap<>();
+        map.put(userOne, new Object());
+        map.put(userTwo, new Object());
+        System.out.println(userOne);
+        System.out.println(userTwo);
+        System.out.println(map);
+    }
 }
